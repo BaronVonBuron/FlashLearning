97b2b3250d8ce09ec3edfd121be471b09d9ca0ab
BaronVonBuron <andbur01@easv365.dk>
2024-01-18 19:40:02 +0100

    Fået det hele rettet til, så det er nogenlunde pænt igen.

---
+++ src/main/java/com/example/flashlearning/AddEditFlashcardMenuController.java
@@ -0,0 +1,34 @@
+package com.example.flashlearning;
+
+import javafx.event.ActionEvent;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.AnchorPane;
+
+public class AddEditFlashcardMenuController {
+
+
+    public AnchorPane AddEditFlashcardWindow;
+    public ImageView AddEditImageView;
+    public Button ChooseImageButton;
+    public TextField QuestionTextField;
+    public TextField AnswerTextField;
+    public TextField BonusinfoTextField;
+    public Button SaveButton;
+    public Button CancelButton;
+    public TextField DecknameTextField;
+    public TextField ImageNameTextField;
+
+    public void ChooseImageFileButtonPressed(ActionEvent actionEvent) {
+
+    }
+
+    public void SaveFlashcardButtonPressed(ActionEvent actionEvent) {
+
+    }
+
+    public void CancelButtonPressed(ActionEvent actionEvent) {
+
+    }
+}
---
+++ src/main/java/com/example/flashlearning/UserMenuController.java
@@ -0,0 +1,37 @@
+package com.example.flashlearning;
+
+import javafx.event.ActionEvent;
+import javafx.scene.control.Button;
+import javafx.scene.control.ListView;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.AnchorPane;
+
+public class UserMenuController {
+    public AnchorPane UserMenuPane;
+    public ListView UserListView;
+    public Button SelectSelectedUserButton;
+    public Button DeleteSelectedUserButton;
+    public TextField UserNameInputField;
+    public Button NewUserButton;
+
+    public void initialize(){
+        //TODO get users loaded into list
+        //TODO make fields and return values to logic, to CRUD user.
+        //TODO make it update all the time.
+    }
+
+    public void SelectSelectedUserButtonPressed(ActionEvent actionEvent) {
+
+    }
+
+    public void DeleteSelectedUserButtonPressed(ActionEvent actionEvent) {
+
+
+    }
+
+    public void NewUserButtonPressed(ActionEvent actionEvent) {
+
+    }
+
+
+}
